b979b3a5f92d753c93a5e1f78e0ebe20bd47a129
Chokanan5599 <39435705+Chokanan5599@users.noreply.github.com>
2024-06-11 21:46:24 +0700

    first commit

---
+++ TLA_SalseForeCast/Controllers/Function_TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TLA_SalseForeCast.Controllers
+{
+    public class Function_TestController : Controller
+    {
+        public IActionResult Demo()
+        {
+            return View();
+        }
+    }
+}
